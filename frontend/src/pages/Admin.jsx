bcc99b57f6ee8a1505155084d2bc84e596f529ad
Morthius <morten_2104@hotmail.com>
2025-04-29 12:25:57 +0200

    wip

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,5 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Admin({ token, logout }) {
+  const [isValidated, setIsValidated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
   console.log(token);
+  useEffect(() => {
+    if (!token) {
+      navigate("/admin");
+    }
+  }, [isValidated, token, navigate]);
+
+  const checkToken = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/Login/validateJWT",
+        {
+          method: "GET",
+          headers: {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        }
+      );
+
+      // Vellykket innlogging
+      if (response.ok) {
+        const data = await response.json();
+        if (data.token) navigate("/admin");
+      } else {
+        // Mislykket innlogging
+        alert("Feil brukernavn eller passord");
+      }
+    } catch (error) {
+      alert("Noe gikk galt ved innlogging");
+      console.error(error);
+      navigate("/login");
+    }
+  };
   return (
     <>
       <div>Admin</div>
