2731623e67dce65b27621658e53ea26096f52341
Morthius <61186981+mrmorthius@users.noreply.github.com>
2025-04-30 09:51:23 +0200

    Docker-compose update and workflow (#8)
    * Added routing to frontend
    * added loginFunctionality and changed servicenames
    * Remove WeatherForecastController file
    * Added login-pages
    * added cors for enviroments
    * Update test user password to hashed format
    * Added logic for JWT validation
    * Added volumes to docker-compose and updated workflow

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,6 +3,9 @@ using backend.Data;
 using Microsoft.EntityFrameworkCore;
 using backend.Repositories;
 using backend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,7 +41,26 @@ builder.Services.AddDbContext<RasteplassDbContext>(options =>
     options.UseMySql(connectionString, serverVersion);
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JWT_ISSUER"],
+        ValidAudience = builder.Configuration["JWT_AUDIENCE"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["JWT_KEY"]))
+    };
+});
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
 
